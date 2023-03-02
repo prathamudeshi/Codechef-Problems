fa1a433083e69ba4263d424f7a446095e7f11b91
prathamudeshi <63914650+prathamudeshi@users.noreply.github.com>
2023-03-02 18:02:50 +0530

    Create Battle ready.cpp

---
+++ Battle ready.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n,t,a=0,b=0;
+	cin>>t;
+	while(t--)
+	{
+	    cin>>n;
+	    if(n%2==0)
+	    {
+	        a++;
+	    }
+	    else
+	    b++;
+	}
+	if(a>b)
+	cout<<"READY FOR BATTLE"<<endl;
+	else
+	cout<<"NOT READY"<<endl;
+	return 0;
+}
