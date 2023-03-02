6c98834af853ff27ca1c487c826936cbe6f8a931
prathamudeshi <63914650+prathamudeshi@users.noreply.github.com>
2023-03-02 23:40:17 +0530

    Create Squats.cpp

---
+++ Squats.cpp
@@ -0,0 +1,14 @@
+//https://www.codechef.com/problems/SQUATS
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t,x;
+	cin>>t;
+	while(t--)
+	{
+	    cin>>x;
+	    cout<<x*15<<endl;
+	}
+	return 0;
+}
