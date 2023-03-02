faf907e5cd24d2a213b172457d8cef215939f3b6
prathamudeshi <63914650+prathamudeshi@users.noreply.github.com>
2023-03-02 09:34:35 +0530

    Create Best of two.cpp

---
+++ Best of two.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int t,x,y;
+    cin>>t;
+    while(t--)
+    {
+        cin>>x>>y;
+        if(x>y)
+        {
+            cout<<x<<endl;;
+        }
+        else
+        cout<<y<<endl;
+    }
+}
