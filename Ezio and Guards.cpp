c3d3a01b3c790662aaa988e160d14ada3a757ae6
prathamudeshi <63914650+prathamudeshi@users.noreply.github.com>
2023-03-02 23:33:22 +0530

    Create Ezio and Guards.cpp

---
+++ Ezio and Guards.cpp
@@ -0,0 +1,14 @@
+//https://www.codechef.com/problems/MANIPULATE
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t,x,y;
+	cin>>t;
+	while(t--)
+	{
+	    cin>>x>>y;
+	    cout<<(x>=y?"YES":"NO")<<endl;
+	}
+	return 0;
+}
