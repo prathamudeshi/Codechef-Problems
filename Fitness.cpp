2ef61e69045e8d60c9c381c4522e4d0a5f98b006
prathamudeshi <63914650+prathamudeshi@users.noreply.github.com>
2023-03-02 23:24:30 +0530

    Create Fitness.cpp

---
+++ Fitness.cpp
@@ -0,0 +1,14 @@
+//https://www.codechef.com/problems/FIT
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t,x;
+	cin>>t;
+	while(t--)
+	{
+	    cin>>x;
+	    cout<<10*x<<endl;
+	}
+	return 0;
+}
