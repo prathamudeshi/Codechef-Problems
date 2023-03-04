b1c836eb21d478fb1f8188e630abb65c39514862
prathamudeshi <63914650+prathamudeshi@users.noreply.github.com>
2023-03-04 23:04:18 +0530

    Create Chef on date.cpp

---
+++ Chef on date.cpp
@@ -0,0 +1,14 @@
+//https://www.codechef.com/problems/CHEFONDATE
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t,x,y;
+	cin>>t;
+	while(t--)
+	{
+	    cin>>x>>y;
+	    cout<<(x>=y?"YES":"NO")<<endl;
+	}
+	return 0;
+}
