2063d341a56691f238d454fedc38d3ebd4cf7815
prathamudeshi <63914650+prathamudeshi@users.noreply.github.com>
2023-03-12 18:04:20 +0530

    Create

---
+++ Podium Finish.cpp
@@ -0,0 +1,14 @@
+//https://www.codechef.com/problems/PODIUM
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t,a,b;
+	cin>>t;
+	while(t--)
+	{
+	    cin>>a>>b;
+	    cout<<a+b<<endl;
+	}
+	return 0;
+}
