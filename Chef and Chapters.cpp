a6ca44a9d5155e138a122d38834985af77cac675
prathamudeshi <63914650+prathamudeshi@users.noreply.github.com>
2023-03-16 22:18:06 +0530

    Create

---
+++ Chef and Chapters.cpp
@@ -0,0 +1,14 @@
+//https://www.codechef.com/problems/SEMCOURSES
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t,x,y,z;
+	cin>>t;
+	while(t--)
+	{
+	    cin>>x>>y>>z;
+	    cout<<x*y*z<<endl;
+	}
+	return 0;
+}
