d4702bfdd3b70b3ce1bc9e896d8bb4e15362e448
prathamudeshi <63914650+prathamudeshi@users.noreply.github.com>
2023-03-19 21:36:01 +0530

    Create

---
+++ Kitchen Timings.cpp
@@ -0,0 +1,14 @@
+//https://www.codechef.com/problems/KITCHENTIME
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t,x,y;
+	cin>>t;
+	while(t--)
+	{
+	    cin>>x>>y;
+	    cout<<y-x<<endl;
+	}
+	return 0;
+}
