f980642ab6bb2b1377a5723b7f0c7751ea8d4dfa
Pratham <udeshipratham@gmail.com>
2023-04-02 13:16:13 +0530

    Folder name change

--- 2001000 Difficulty/Enough Space.py
+++
@@ -1,8 +0,0 @@
-#https://www.codechef.com/problems/ENSPACE?tab=statement
-t=int(input())
-for x in range(t):
-    n,x,y=map(int,input().split())
-    if(n-x-(2*y)>=0):
-        print("YES")
-    else:
-        print("NO")
--- 2001000 Difficulty/Final Population.py
+++
@@ -1,5 +0,0 @@
-#https://www.codechef.com/problems/POPULATION
-t=int(input())
-for a in range(t):
-    x,y,z=map(int,input().split())
-    print(x-y+z)
