3c0485227564990425b28c3ea9e40dc9dd0bb935
Pratham <udeshipratham@gmail.com>
2023-06-30 13:05:02 +0530

    Add Jerry Chase:

---
+++ 200-1000 Difficulty/Jerry Chase.cpp
@@ -0,0 +1,15 @@
+//https://www.codechef.com/problems/JERRYCHASE
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int x,y;
+	    cin>>x>>y;
+	    cout<<((x>=y)?"NO":"YES")<<endl;
+	}
+	return 0;
+}
