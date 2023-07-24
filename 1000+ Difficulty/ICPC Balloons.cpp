359b387ae48bda1efb25afd8d728f67616da6a73
Pratham <udeshipratham@gmail.com>
2023-07-24 22:04:56 +0530

    .

--- 1000+ Difficulty/ICPC Balloons.cpp
+++ 1000+ Difficulty/ICPC Balloons.cpp
@@ -1,36 +1,20 @@
 //https://www.codechef.com/problems/BALLOON
-#include <iostream>
+#include<bits/stdc++.h>
 using namespace std;
 
-int main() {
-	int t;
-	cin>>t;
-	while(t--)
-	{
-	    int n,a=0;
-	    bool ans=false;
-	    cin>>n;
-	    int arr[n];
-	    int vib[7]={1,2,3,4,5,6,7};
-	    for(int i=0;i<n;i++)
-	    {
-	        cin>>arr[i];
-	        for(int j=0;j<7;j++)
-	        {
-	            if(arr[i]==vib[j])
-	            {
-	                vib[j]==0;
-	                a++;
-	            }
-	            if(a==7)
-	                ans=true;
-	        }
-	        if(ans==true)
-	        {
-	            cout<<i+1<<endl;
-	            break;
-	        }
-	    }
-	}
-	return 0;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n,ans;
+        cin>>n;
+        int arr[n];
+        for(int i=0;i<n;i++){
+            cin>>arr[i];
+            if(arr[i]>=1 && arr[i]<=7)
+                ans=i;
+        }
+        cout<<ans+1<<endl;
+    }
+    return 0;
 }
