379273fb52e315f462ad654d074793e7a187b6a1
Pratham Udeshi <63914650+prathamudeshi@users.noreply.github.com>
2025-01-30 01:06:52 +0530

    Create SQUIDBANK.cpp

---
+++ Contest Problems/SQUIDBANK.cpp
@@ -0,0 +1,11 @@
+// https://www.codechef.com/START171D/problems/SQUIDBANK
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int n,k;
+    cin>>n>>k;
+    cout<<(n-k)*10000<<endl;
+}
