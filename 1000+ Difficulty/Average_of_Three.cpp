155aee319db50d79805e3cea1390765c887d1d0b
Pratham Udeshi <63914650+prathamudeshi@users.noreply.github.com>
2025-03-08 22:51:49 +0530

    Create Average_of_Three.cpp

---
+++ 1000+ Difficulty/Average_of_Three.cpp
@@ -0,0 +1,15 @@
+// https://www.codechef.com/practice/course/1-star-difficulty-problems/DIFF1200/problems/AVGOF3
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        cout<<n-1<<" "<<n<<" "<<n+1<<endl;
+    }
+}
