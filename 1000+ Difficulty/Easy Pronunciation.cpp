943c5602ab929bc043d1e288a94b299f3c91a177
Pratham Udeshi <63914650+prathamudeshi@users.noreply.github.com>
2025-05-10 17:58:22 +0530

    Update Easy Pronunciation.cpp

--- 1000+ Difficulty/Easy Pronunciation.cpp
+++ 1000+ Difficulty/Easy Pronunciation.cpp
@@ -1,26 +1,32 @@
 //https://www.codechef.com/problems/EZSPEAK
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
 int main() {
+	// your code goes here
 	int t;
 	cin>>t;
-	while(t--)
-	{
-	    int n,a=0;
+	unordered_set<char> v={'a','e','i','o','u'};
+	while(t--){
+	    int n;
 	    cin>>n;
 	    string s;
 	    cin>>s;
-	    for(int i=0;i<n;i++)
-	    {
-	        if(s[i]=='a' || s[i]=='e' || s[i]=='i' || s[i]=='o' || s[i]=='u')
-	            a=0;
-	        else
-	            a++;
-	        if(a==4)
+	    int c = 0;
+	    for(int i=0;i<n;i++){
+	        c++;
+	        if(v.count(s[i])){
+	            c=0;
+	        }
+	        if(c==4){
+	            cout<<"NO"<<endl;
+	            c=0;
 	            break;
+	        }
+	        if(i==n-1)
+	            cout<<"YES"<<endl;
 	    }
-	    cout<<(a==4?"NO":"YES")<<endl;
 	}
-	return 0;
+
 }
+
